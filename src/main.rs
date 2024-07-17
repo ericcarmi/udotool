a321b2bdd2ba38f71aa9eac73897cb435200a7f5
ericcarmi <ecarmi@protonmail.com>
2024-07-16 17:38:55 -0700

    this works on windows

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,49 @@
+// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// seems to be ok without using line above, and if it is used, help does not get printed
 use clap::Parser;
 use commands::Commands;
 use enigo::{Direction, Enigo, Keyboard, Settings};
 use std::time::Duration;
 mod commands;
 
-/// Simple program to simulate writing text
-/// example string
-/// ("%p "+y) ctrl[down] (%run ) ctrl-v enter
+/// --
+/// simple program to write text and press keys
+/// -- it accepts one long argument separated by spaces
+/// -- to write most strings, surround by [] -- this avoids issues with shells and quotes
+/// -- to write the character ", use the command "quo" (with no quotes around it)
+/// -- other commands represent key presses that do not write text
+/// -- e.g. ctrl-shift-down, ctrl-v
+/// -- example usage
+/// win-dotool -t quo [%p] quo [+yu] ctrl-down [%run ] ctrl-v enter
 #[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
+#[command(version, arg_required_else_help = true)]
 struct Args {
     /// string of commands to be parsed into typing and action keys
-    #[arg(short, long)]
-    text: String,
+    #[arg(short, long,  num_args = 1.., value_delimiter=' ')]
+    text: Vec<String>,
 }
 
 fn main() -> Result<(), String> {
     let mut enigo = Enigo::new(&Settings::default()).unwrap();
     let args = Args::parse();
-    println!("{:?}", args);
     let mut command = "".to_string();
     let mut is_text = false;
     let mut text = "".to_string();
 
+    let trailing: String = format!("{:?}", args.text.join(" ").replace("\\", ""));
+
     let mut keys = vec![];
 
-    let text_space = args.text.to_lowercase() + " ";
+    // let text_space = args.text.unwrap().to_lowercase() + " ";
+    let text_space = trailing.to_lowercase() + " ";
 
     for char in text_space.chars() {
-        if char == ')' {
+        if char == '\\' {
+            continue;
+        }
+        if char == ']' {
             is_text = false;
             write_text(text.clone(), &mut enigo);
-            // println!("{:?}", text);
 
             text = "".to_string();
             continue;
@@ -41,7 +53,7 @@ fn main() -> Result<(), String> {
             continue;
         }
         // text is surrounded by parentheses
-        if char == '(' {
+        if char == '[' {
             // read until right parentheses, if none is reached, return error
             is_text = true;
         }
@@ -50,11 +62,13 @@ fn main() -> Result<(), String> {
             let spl = command.split('-');
             if spl.clone().count() == 1 {
                 if let Some(c) = Commands.get(command.as_str()) {
-                    // println!("{:?}", c);
-                    // keys.push(c);
+                    enigo.key(*c, Direction::Click);
                     command = "".to_string();
                 } else {
-                    return Err("command not recognized, sorry can't continue".to_string());
+                    if command == "quo".to_string() {
+                        write_text("\"".to_string(), &mut enigo);
+                    }
+                    command = "".to_string();
                 }
             } else {
                 // want to skip last, so reverse
@@ -68,15 +82,14 @@ fn main() -> Result<(), String> {
                 if let Some(last) = spl.last() {
                     if let Some(last_cmd) = Commands.get(last) {
                         // keys are held down together, then "last" is pressed, then keys are released
-                        // println!("{:?}", keys);
                         for key in keys.clone() {
                             enigo.key(*key, Direction::Press);
-                            std::thread::sleep(Duration::from_micros(100));
+                            std::thread::sleep(Duration::from_micros(300));
                         }
                         enigo.key(*last_cmd, Direction::Click);
                         for key in keys {
                             enigo.key(*key, Direction::Release);
-                            std::thread::sleep(Duration::from_micros(100));
+                            std::thread::sleep(Duration::from_micros(300));
                         }
 
                         keys = vec![];
@@ -88,7 +101,7 @@ fn main() -> Result<(), String> {
                     return Err("failed to get last part of slice".to_string());
                 }
             }
-        } else if char != ' ' {
+        } else if char != ' ' && char != '\\' && char != '\"' {
             command += char.to_string().as_str();
         }
     }
@@ -99,6 +112,7 @@ fn main() -> Result<(), String> {
 fn write_text(text: String, enigo: &mut Enigo) {
     for t in text.chars() {
         let _r = enigo.text(t.to_string().as_str());
+        std::thread::sleep(Duration::from_millis(1));
     }
     // apparently it needs a delay between writing text and key presses
     std::thread::sleep(Duration::from_millis(10));
